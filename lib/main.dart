4ac5349e20a581b770093cd1ba01adadbea1aee4
Catherine Angel <catheriner40540@gmail.com>
2022-11-29 09:26:49 +0700

    add folders and theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sayang_dibuang_mobile/theme_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -70,11 +71,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: ThemeColor.sand,
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      //   backgroundColor: ThemeColor.darkGreen,
+      // ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
---
+++ lib/theme_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  ThemeColor._();
+
+  static const Color sand = Color(0xFFF6F2E9);
+  static const Color darkGreen = Color(0xFF185641);
+  static const Color gold = Color(0xFFBFA861);
+}
