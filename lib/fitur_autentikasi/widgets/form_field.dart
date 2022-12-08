f2dc30fca42a8a5a7bd89beefd327645ba68be0a
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-12-08 14:26:33 +0700

    Add input formatter for number

--- lib/fitur_autentikasi/widgets/form_field.dart
+++ lib/fitur_autentikasi/widgets/form_field.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sayang_dibuang_mobile/fitur_autentikasi/utilities/style.dart';
 
 class TextFormFieldAuth extends StatelessWidget {
-  const TextFormFieldAuth(
-      {super.key,
-      required this.placeholder,
-      required this.setFieldState,
-      this.validator,
-      this.iconButton,
-      this.obscureText = false,
-      this.hintText,
-      this.errorStyle});
+  const TextFormFieldAuth({
+    super.key,
+    required this.placeholder,
+    required this.setFieldState,
+    this.validator,
+    this.iconButton,
+    this.obscureText = false,
+    this.hintText,
+    this.errorStyle,
+    this.numberOnly = false,
+  });
 
   final String placeholder;
   final dynamic setFieldState;
@@ -19,17 +22,23 @@ class TextFormFieldAuth extends StatelessWidget {
   final bool obscureText;
   final String? hintText;
   final TextStyle? errorStyle;
+  final bool numberOnly;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-        obscureText: obscureText,
-        style: const TextStyle(fontFamily: "PlusJakarta'", fontSize: 15),
-        decoration: inputDecorationForm(placeholder,
-            iconButton: iconButton, hintText: hintText, errorStyle: errorStyle),
-        onChanged: setFieldState,
-        onSaved: setFieldState,
-        validator: validator);
+      keyboardType: (numberOnly) ? TextInputType.number : null,
+      inputFormatters: (numberOnly)
+          ? <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly]
+          : null,
+      obscureText: obscureText,
+      style: const TextStyle(fontFamily: "PlusJakarta'", fontSize: 15),
+      decoration: inputDecorationForm(placeholder,
+          iconButton: iconButton, hintText: hintText, errorStyle: errorStyle),
+      onChanged: setFieldState,
+      onSaved: setFieldState,
+      validator: validator,
+    );
     ;
   }
 }
