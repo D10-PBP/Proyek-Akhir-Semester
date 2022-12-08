dc54e8f9248adeb33c23bf46cf353b32d954fb1a
gibranoderenz <gibranoderenz123@gmail.com>
2022-12-08 16:38:42 +0700

    Add crowdfundings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:flutter/services.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:sayang_dibuang_mobile/barang_bekas/pages/beranda.dart';
-import 'package:sayang_dibuang_mobile/crowdfunding/pages/crowdfundings.dart';
-import 'package:sayang_dibuang_mobile/fitur_autentikasi/pages/profile.dart';
-
-import 'core/theme/theme_color.dart';
+import 'package:sayang_dibuang_mobile/base.dart';
+import 'package:sayang_dibuang_mobile/core/providers/page_provider.dart';
 import 'package:sayang_dibuang_mobile/fitur_autentikasi/providers/current_user_profile.dart';
 
 void main() {
@@ -24,96 +19,12 @@ class MyApp extends StatelessWidget {
       providers: [
         Provider(create: (_) => CookieRequest()),
         ChangeNotifierProvider(create: (_) => CurrentUserProfileModel()),
+        ChangeNotifierProvider(create: (_) => PageProvider())
       ],
       child: MaterialApp(
-        title: 'Sayang Dibuang',
         theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'PlusJakarta'),
-        home: const MyHomePage(),
+        home: const BasePage(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  final String title = 'Sayang Dibuang App';
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> pages = [
-      CrowdfundingsPage(), // nanti diganti sama Home
-      BerandaBarangPage(),
-      BerandaBarangPage(), // nanti diganti sama Request
-      CrowdfundingsPage(),
-      CrowdfundingsPage(), // nanti diganti sama Leaderboard
-      ProfilePage(),
-    ];
-
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          systemNavigationBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.dark,
-          statusBarBrightness: Brightness.dark),
-      child: Scaffold(
-          // background color
-          backgroundColor: ThemeColor.sand,
-
-          // the screen
-          body: pages[currentIndex],
-
-          // the bottom navbar
-          bottomNavigationBar: Container(
-            color: ThemeColor.gold,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-              child: GNav(
-                  gap: 8,
-                  backgroundColor: ThemeColor.gold,
-                  color: ThemeColor.sand,
-                  activeColor: ThemeColor.sand,
-                  tabBackgroundColor: ThemeColor.darkGreen,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                  onTabChange: (index) {
-                    setState(() {
-                      currentIndex = index;
-                    });
-                  },
-                  tabs: const [
-                    GButton(
-                      icon: Icons.home_rounded,
-                      text: 'Home',
-                    ),
-                    GButton(
-                      icon: Icons.dashboard,
-                      text: 'Beranda',
-                    ),
-                    GButton(
-                      icon: Icons.handshake_rounded,
-                      text: 'Request',
-                    ),
-                    GButton(
-                      icon: Icons.group_outlined,
-                      text: 'Crowdfunding',
-                    ),
-                    GButton(
-                      icon: Icons.leaderboard_rounded,
-                      text: 'Leaderboard',
-                    ),
-                    GButton(
-                      icon: Icons.account_circle_outlined,
-                    ),
-                  ]),
-            ),
-          )),
     );
   }
 }
